ac9635b46c3a2eeb6091ef2ed1fa98d99059bc9d
Ausdauer1 <steady051@naver.com>
2022-02-11 21:03:33 +0900

    게시글 관련 1차본

--- routes/posts.js
+++ routes/posts.js
@@ -1,25 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authMiddleware = require('../middlewares/auth-middleware');
-const controller = require('../controller/posts');
+const authMiddleware = require("../middlewares/auth-middleware");
+const controller = require("../controller/posts");
 
 // 게시물 모두 보여주기 (메인페이지)
-router.get('/post_list', controller.getPosts);
+router.get("/post_list", controller.getPosts);
 
 // 게시물 생성
-router.post('/post', authMiddleware, controller.makePosts);
+router.post("/post", authMiddleware, controller.makePosts);
 
 // 게시물 상세페이지
-router.get('/detail/:postId', controller.detailPosts);
+router.get("/detail/:postId", controller.detailPosts)
 
 // 게시물 수정하기를 들어갈 수 있는 권한 확기 (수정)
-router.get('/detail/gomodify/:postId', authMiddleware, controller.goModifyPosts);
+router.get("/detail/gomodify/:postId", authMiddleware, controller.goModifyPosts)
 
 // 게시물 수정하기 완료 버튼을 눌렀을 때
-router.put('/update/:postId', authMiddleware, controller.modifyPosts);
+router.put("update/:postId", authMiddleware, controller.modifyPosts)
 
-// 게시물 삭제하기 
-router.put('/delete/:postId', authMiddleware, controller.deletePosts);
-
+// 게시물 삭제하기
+router.put("delete/:postId", authMiddleware, controller.deletePosts)
 
 module.exports = router;
