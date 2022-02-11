a161bbe46366d1c983619da56aa162a2cee9ef9a
Ausdauer1 <steady051@naver.com>
2022-02-11 20:14:12 +0900

    autoincrease 다운

--- controller/posts.js
+++ controller/posts.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const Posts = require("../models/posts");
+const express = require('express');
+const Posts = require('../models/posts');
 
 // 메인페이지로 줄 정보 (게시물 목록) - 50완
 module.exports.getPosts = async (req, res) => {
@@ -20,56 +20,63 @@ module.exports.makePosts = async (req, res) => {
     const { title, thumbnail, contents } = req.body;
 
     await Posts.create({
-      userId: user.userId,
+      userId: user.loginId,
       thumbnail,
       contents,
       title,
     });
 
-    res.json({ ok: "true" });
+    res.json({ ok: 'true' });
   } catch (error) {
-    res.status(400).json({ ok: "false" });
+    res.status(400).json({ ok: 'false' });
   }
 };
 
-// 상세페이지
+// 상세페이지 - 완료
 module.exports.detailPosts = async (req, res) => {
-  const {detailId}  = req.params;
+  const { postId } = req.params;
 
-    const articles = await Articles.findOne({ id: Number(detailId) })
+  const posts = await Posts.findOne({ id: Number(postId) });
 
-    res.json({
-        articles
-    });
-}
+  res.json({
+    posts,
+  });
+};
 // 수정하기를 들어갈 수 있는 권한확인 (수정하기 버튼을 눌렀을 때)
-module.exports.detailPosts = async (req, res) => {
-  const {detailId}  = req.params;
+module.exports.goModifyPosts = async (req, res) => {
+  const { user } = res.locals;
+  const { detailId }  = req.params;
 
-    const articles = await Articles.findOne({ id: Number(detailId) })
+  const [articles] = await Articles.find({ id: Number(detailId) })
 
-    res.json({
-        articles
-    });
-}
-// 게시물 수정
-module.exports.detailPosts = async (req, res) => {
-  const {detailId}  = req.params;
+  if (articles.nickname === user.nickname) {
+        res.json({
+            result : "success"
+        });
+  } else {
+        res.json({ 
+            result : "fail"
+        });
+  }}
 
-    const articles = await Articles.findOne({ id: Number(detailId) })
+// 게시물 수정 (수정완료 버튼을 누럿을 때)
+module.exports.modifyPosts = async (req, res) => {
+  const { detailId } = req.params;
 
-    res.json({
-        articles
-    });
-}
+  const articles = await Articles.findOne({ id: Number(detailId) });
+
+  res.json({
+    articles,
+  });
+};
 
 // 게시물 삭제
-module.exports.detailPosts = async (req, res) => {
-  const {detailId}  = req.params;
+module.exports.deletePosts = async (req, res) => {
+  const { detailId } = req.params;
 
-    const articles = await Articles.findOne({ id: Number(detailId) })
+  const articles = await Articles.findOne({ id: Number(detailId) });
 
-    res.json({
-        articles
-    });
-}
+  res.json({
+    articles,
+  });
+};
--- routes/posts.js
+++ routes/posts.js
@@ -5,10 +5,13 @@ const controller = require("../controller/posts");
 
 // 게시물 모두 보여주기 (메인페이지)
 router.get("/post_list", authMiddleware, controller.getPosts);
-// 게시물 작성 
-router.post("/post", authMiddleware, controller.makePosts);
-// 게시물 수정
-router.put()
-// 게시물 삭제
 
+// 게시물 생성
+router.post("/post", authMiddleware, controller.makePosts);
+
+// 게시물 상세페이지
+router.get("/detail/:postId", controller.detailPosts)
+
+// 게시물 수정하기를 들어갈 수 있는 권한 확기 (수정)
+router.get("/detail/:postId", controller.goModifyPosts)
 module.exports = router;
