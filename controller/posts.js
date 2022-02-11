e5a5360c2ecc825495d27654ec2a060d03de8b97
Ausdauer1 <steady051@naver.com>
2022-02-11 20:29:16 +0900

    제 방식 피리티어..

--- controller/posts.js
+++ controller/posts.js
@@ -20,7 +20,7 @@ module.exports.makePosts = async (req, res) => {
     const { title, thumbnail, contents } = req.body;
 
     await Posts.create({
-      userId: user.loginId,
+      loginId: user.loginId,
       thumbnail,
       contents,
       title,
@@ -36,38 +36,46 @@ module.exports.makePosts = async (req, res) => {
 module.exports.detailPosts = async (req, res) => {
   const { postId } = req.params;
 
-  const posts = await Posts.findOne({ id: Number(postId) });
+  const post = await Posts.findOne({ id: Number(postId) });
 
   res.json({
-    posts,
+    post,
   });
 };
 // 수정하기를 들어갈 수 있는 권한확인 (수정하기 버튼을 눌렀을 때)
 module.exports.goModifyPosts = async (req, res) => {
   const { user } = res.locals;
-  const { detailId }  = req.params;
+  const { postId } = req.params;
 
-  const [articles] = await Articles.find({ id: Number(detailId) })
+  const post = await Posts.findOne({ id: Number(postId) });
 
-  if (articles.nickname === user.nickname) {
-        res.json({
-            result : "success"
-        });
+  if (post.loginId === user.loginId) {
+    res.json({
+      ok: 'true',
+    });
   } else {
-        res.json({ 
-            result : "fail"
-        });
-  }}
+    res.json({
+      ok: 'false',
+    });
+  }
+};
 
 // 게시물 수정 (수정완료 버튼을 누럿을 때)
 module.exports.modifyPosts = async (req, res) => {
   const { detailId } = req.params;
+  const { user } = res.locals;
+  const { comment, title, date } = req.body;
 
-  const articles = await Articles.findOne({ id: Number(detailId) });
+  const existsarticle = await Articles.findOne({ id: Number(detailId) });
 
-  res.json({
-    articles,
-  });
+  if (existsarticle.nickname === user.nickname) {
+    await Articles.updateOne(
+      { id: Number(detailId) },
+      { $set: { comment, title, date } }
+    );
+
+    res.json({ result: 'success' });
+  }
 };
 
 // 게시물 삭제
--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const authMiddleware = require("../middlewares/auth-middleware");
 const controller = require("../controller/posts");
 
 // 게시물 모두 보여주기 (메인페이지)
-router.get("/post_list", authMiddleware, controller.getPosts);
+router.get("/post_list", controller.getPosts);
 
 // 게시물 생성
 router.post("/post", authMiddleware, controller.makePosts);
@@ -13,5 +13,8 @@ router.post("/post", authMiddleware, controller.makePosts);
 router.get("/detail/:postId", controller.detailPosts)
 
 // 게시물 수정하기를 들어갈 수 있는 권한 확기 (수정)
-router.get("/detail/:postId", controller.goModifyPosts)
+router.get("/detail/:postId", authMiddleware, controller.goModifyPosts)
+
+// 게시물 수정하기 완료 버튼을 눌렀을 때
+
 module.exports = router;
