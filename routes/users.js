9ef6384c619933eea3ff65d1d211d417601db425
Haksae <95196634+Haksae90@users.noreply.github.com>
2022-02-11 17:54:12 +0900

    update users (#5)

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require("../middlewares/auth-middleware");
 
 const {
-
+    signup,
+    login,
+    auth
 } = require("../controller/users");
 
 // 회원가입
+router.post('/signup', signup);
 
 // 로그인
+router.post('/login', login);
 
 // 로그인 확인
-
-// api 확인
+router.get('/auth', authMiddleware, auth)
 
 module.exports = router;
