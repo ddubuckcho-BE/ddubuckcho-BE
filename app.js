e697b96457cbd95fbcaea40eed3a54d5d49cfd3f
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-02-12 19:31:17 +0900

    feat(dotenv) v2

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(cors()); // 빈칸으로 두면 모든 요청 허용
 app.use(requestMiddleware);
 
 app.use('/api', [usersRouter, postsRouter]);
-app.use(express.static('test'));
 
 app.listen(port, () => {
     console.log(port, '포트로 서버가 켜졌습니다.')
