fc65239b62a018045ef53a2656a4fe28dbfa0113
Ausdauer1 <steady051@naver.com>
2022-02-13 01:26:14 +0900

    feat0213(9)

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
   mongoose
-    .connect(process.env.MONGO_URL, {
+    .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/ddubuckcho', {
       ignoreUndefined: true,
     })
     .catch((error) => {
