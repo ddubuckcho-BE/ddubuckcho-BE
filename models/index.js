6a828bdebacd17a40509d88fa909c07bb0ca6eb3
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-02-14 16:31:51 +0900

    "update mongoose"

--- models/index.js
+++ models/index.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connect = () => {
   mongoose
     .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/ddubuckcho', {
-      user: "test",
-      pass: "test",
       ignoreUndefined: true,
     })
     .catch((error) => {
