3815978f86184e6125943961c8f8105d340b2977
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-02-14 18:07:07 +0900

    test db

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
   mongoose
-    .connect(process.env.MONGO_URL || 'mongodb://test:test@localhost:27017/ddubuckcho', {
+    .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/ddubuckcho', {
       ignoreUndefined: true,
     })
     .catch((error) => {
