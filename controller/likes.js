7caffc22d2132539dcf55ded4c67efa30242a5af
Ausdauer1 <steady051@naver.com>
2022-02-15 17:53:41 +0900

    feat0215(32)

--- controller/likes.js
+++ controller/likes.js
@@ -5,14 +5,19 @@ module.exports.makeLikes = async (req, res) => {
     const { user } = res.locals;
     const { post_id } = req.params;
     console.log(post_id)
-
+    
+    const thePost = await Posts.findOne({id: Number(post_id)})
+    const count = thePost.like_count
+    const id = thePost.like_id
+    console.log(count, id)
+    
     await Posts.updateOne(
       { id: Number(post_id) },
-      { $set: { like_count: this += 1 , /*like_id:like_id.push(user.loginId)*/} 
+      { $set: { like_count: count += 1 , like_id: id.push(user.loginId)} 
     });
     
-    const thePost  = await Posts.findOne({id: Number(post_id)})
-    const like_count = thePost.like_count
+    const findPost  = await Posts.findOne({id: Number(post_id)})
+    const like_count = findPost.like_count
 
     console.log(like_count)
     res.json({ like_count });
