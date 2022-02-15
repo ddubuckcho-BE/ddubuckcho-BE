1d1b98907688766c5e0e728a5ad80d7997f0d5ed
Ausdauer1 <steady051@naver.com>
2022-02-15 21:04:30 +0900

    feat0215(38)

--- middlewares/main-middleware.js
+++ middlewares/main-middleware.js
@@ -5,19 +5,12 @@ module.exports = (req, res, next) => {
   // authoriztion 참조
   const { authorization } = req.headers;  // 프론트에서 대문자로 보내도 여기서는 소문자로 변환됨
 
-  if (authorization.length < 10) {  
+  if (!authorization) {  
         console.log(authorization)
         next();
   } else {
     
-    const [tokenType, tokenValue] = authorization.split(' '); // 공백을 기준으로 배열을 반환
-    
-    if (tokenType !== 'Bearer') {
-        res.status(401).send({
-          errorMessage: '로그인 후 사용하세요',
-        });
-        return;
-      }
+    const tokenValue = authorization // 공백을 기준으로 배열을 반환
 
     try {
         const { userId } = jwt.verify(tokenValue, process.env.TOKENKEY); // 디코드에서 loginId 값만 암호화했으니
